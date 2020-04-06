e2fffb6e14492041001b6b636835d4b7d79e10b3
Daniel Fries <rhinodash@gmail.com>
2020-04-06 15:23:04 -0400

    Add images folder and basic logo image

--- StockMarketWebApp/frontend/src/App.vue
+++ StockMarketWebApp/frontend/src/App.vue
@@ -1,6 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
+   <div id="app">
+   <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <router-link to="/" class="navbar-brand">
+   Logo attribution: logo PNG Designed By haris99 from "https://pngtree.com/" Pngtree.com</a> 
+  <img id="img-logo-nav" />
+  Stock Market Game
+  </router-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      Navbar text with an inline element
+    </span>
+  </div>
+</nav> -->
+
+
+
+
+     <div id="nav">
       <div v-if="user">
         Logged in
         <div>
@@ -47,3 +77,7 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+
+</style>

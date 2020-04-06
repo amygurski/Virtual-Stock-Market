70dfd70acea8f706af2d9b23b50fb8cc73f7c776
Daniel Fries <rhinodash@gmail.com>
2020-04-06 15:54:10 -0400

    Fix router view missing from App.vue

--- StockMarketWebApp/frontend/src/App.vue
+++ StockMarketWebApp/frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
    <div id="app">
-   <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <router-link to="/" class="navbar-brand">
-   Logo attribution: logo PNG Designed By haris99 from "https://pngtree.com/" Pngtree.com</a> 
-  <img id="img-logo-nav" />
+    <!-- Logo attribution: logo PNG Designed By haris99 from "https://pngtree.com/" Pngtree.com  -->
+  <img src="require(./images/StockMarketLogo.png)" id="img-logo-nav" />
   Stock Market Game
   </router-link>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,13 +24,14 @@
     <span class="navbar-text">
       Navbar text with an inline element
     </span>
+    
   </div>
-</nav> -->
+</nav>
+<router-view />
 
 
 
-
-     <div id="nav">
+     <!-- <div id="nav">
       <div v-if="user">
         Logged in
         <div>
@@ -47,7 +48,7 @@
       </div>
       <router-link to="/">Home</router-link>
     </div>
-    <router-view />
+    <router-view /> -->
   </div>
 </template>
 

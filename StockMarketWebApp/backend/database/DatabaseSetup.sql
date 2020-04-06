4ee4ac74d132a2351df20506d465c49e55289fb9
Daniel Fries <rhinodash@gmail.com>
2020-04-06 14:15:15 -0400

    Refactor sql script

---
+++ StockMarketWebApp/backend/database/DatabaseSetup.sql
@@ -0,0 +1,44 @@
+﻿
+-- Switch to the system (aka master) database
+USE master;
+GO
+
+-- Delete the Stock Market Database (IF EXISTS)
+IF EXISTS(select * from sys.databases where name='StockMarketDB')
+DROP DATABASE StockMarketDB;
+GO
+
+-- Create a new Stock Market Database
+CREATE DATABASE StockMarketDB;
+GO
+
+-- Switch to the Stock Market Database
+USE StockMarketDB
+GO
+
+BEGIN TRANSACTION;
+
+CREATE TABLE users
+(
+	id			int			identity(1,1),
+	username	varchar(50)	not null,
+	password	varchar(50)	not null,
+	salt		varchar(50)	not null,
+	role		varchar(50)	default('user'),
+
+	constraint pk_users primary key (id)
+);
+
+
+COMMIT TRANSACTION;
+
+-- Seed Database Section
+BEGIN TRANSACTION;
+
+-- default username of 'user' and default password of 'greatwall'
+INSERT INTO users
+  (username,password,salt,role)
+VALUES
+  ('user', 'jUE98uhvS5tdIlxRsmz1W7/Qaqo=', '9CWPUTvXqQ4=', 'User');
+
+COMMIT TRANSACTION;

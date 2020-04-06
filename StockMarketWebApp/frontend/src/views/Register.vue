9f4edbeeb27cbfd299b06ed0e0dce2fbdfa1622d
Amy Gurski <amygurski@gmail.com>
2020-04-06 15:07:35 -0400

    Adds basic styling to login and register forms

--- StockMarketWebApp/frontend/src/views/Register.vue
+++ StockMarketWebApp/frontend/src/views/Register.vue
@@ -5,6 +5,8 @@
       <div class="alert alert-danger" role="alert" v-if="registrationErrors">
         There were problems registering this user.
       </div>
+
+      <div class="form-group">
       <label for="username" class="sr-only">Username</label>
       <input
         type="text"
@@ -15,6 +17,8 @@
         required
         autofocus
       />
+      </div>
+      <div class="form-group">
       <label for="password" class="sr-only">Password</label>
       <input
         type="password"
@@ -24,6 +28,8 @@
         v-model="user.password"
         required
       />
+      </div>
+      <div class="form-group">
       <input
         type="password"
         id="confirmPassword"
@@ -32,12 +38,18 @@
         v-model="user.confirmPassword"
         required
       />
-      <router-link :to="{ name: 'login' }">
-        Have an account?
-      </router-link>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Create Account
-      </button>
+      </div>
+      <div class="form-group">
+        <button class="btn btn-lg btn-primary btn-block" type="submit">
+          Create Account
+        </button>
+      </div>
+      <div class="form-group">
+        <router-link :to="{ name: 'login' }">
+          Have an account?
+        </router-link>
+      </div>
+
     </form>
   </div>
 </template>
@@ -81,4 +93,9 @@ export default {
 </script>
 
 <style>
+  .form-register{
+    max-width: 500px;
+    margin: auto;
+    padding: 2rem;
+  }
 </style>

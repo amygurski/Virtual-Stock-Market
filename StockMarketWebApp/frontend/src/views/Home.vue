22df312872f07c6cd0a3599f936eeccffb055e6c
Chelsea Williams <w.chelsea64@gmail.com>
2020-04-07 09:47:11 -0400

    Home.vue with Centered Text and Photo

--- StockMarketWebApp/frontend/src/views/Home.vue
+++ StockMarketWebApp/frontend/src/views/Home.vue
@@ -8,11 +8,12 @@
 
 <template>
 
-<div class="container">
+<div class="container" >
   <div class="intro text">
+     <h1>Welcome to the <em><strong>Virtual Stock Market!</strong></em></h1>
   <img src="/images/WallStreet.jfif" id="img-wall-street" />
    <!-- add require later. I want this to be the background image...eventually -->
-    <h1>Welcome to the <strong>Virtual Stock Market!</strong></h1>
+   
     <h2>Want to practice trading, but don't want to risk your own money?</h2>
     <h3>Play games and practice with us! You can play 'lone-wolf' style or compete with friends.</h3>
   </div>
@@ -28,17 +29,23 @@ export default {
 <style>
 .container {
     max-width: 1200px;
-    margin: auto;
+    margin: 0 auto;
+    text-align: center;
     padding: 7rem;
     font-size: 20px;
+  
 }
 
-.h1 {
-  padding-bottom: 10px;
+h1 {
+  padding-bottom: 30px;
 }
 
-.h2 {
-  padding-bottom: 15px;
+h2 {
+  padding: 15px;
 }
+img {
+  opacity: 0.7;
+}
+
 
 </style>

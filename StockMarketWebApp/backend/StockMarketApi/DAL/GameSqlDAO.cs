74a6bebb3a21f6f1cd28b95186d8a0dce0a55d48
Daniel Fries <rhinodash@gmail.com>
2020-04-07 14:20:59 -0400

    Add Game Class

---
+++ StockMarketWebApp/backend/StockMarketApi/DAL/GameSqlDAO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockMarketApi.DAL
+{
+    /// <summary>
+    /// A SQL DAO for game objects
+    /// </summary>
+    public class GameSqlDAO : IGameDAO
+    {
+        public IList<Game> GetAllActiveGames()
+        {
+            List<Game> result = new List<Game>();
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM GAMES", conn);
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        user = MapRowToUser(reader);
+                    }
+                }
+
+                return user;
+            }
+            catch (SqlException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+    }
+}
---
+++ StockMarketWebApp/backend/StockMarketApi/DAL/IGameDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockMarketApi.DAL
+{
+    /// <summary>
+    /// An interface for game objects
+    /// </summary>
+    public interface IGameDAO
+    {
+        /// <summary>
+        /// Returns all games
+        /// </summary>
+        /// <returns></returns>
+        IList<Game> GetAllGames();
+    }
+}

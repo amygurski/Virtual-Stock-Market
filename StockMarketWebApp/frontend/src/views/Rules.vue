4d371a08395dd9d097b186ce6e01a8846bf3c99e
Allan Morse <despicari@gmail.com>
2020-04-07 09:45:53 -0400

    Rules added, needs styled

--- StockMarketWebApp/frontend/src/views/Rules.vue
+++ StockMarketWebApp/frontend/src/views/Rules.vue
@@ -1,5 +1,28 @@
 <template>
-  <H1>Game Rules</H1>
+  
+  <div class="text-center">
+    
+      <H1 id="h1">Game Rules</H1>
+      
+      <p class="spacer"></p>
+
+      <p id="rules">
+        &nbsp;&nbsp;&nbsp;&nbsp;Welcome to the Virtual Stock Market! As a player, to start you must first either create a new game
+        or join an existing game. Once you have created, or joined a game you will be provided with $100,000.00 of starting
+        money for you to invest into any available stocks of your choice. You and your fellow
+        players have the ability to buy, sell, and research stocks historical trends so that you
+        can make informed choices about your investments. 
+        </p>
+
+        <p id="rules2">
+        &nbsp;&nbsp;&nbsp;&nbsp;The length of the game will be determined at creation of the game session
+        and players will have until the end date of the game to make as much money as possible. When the game ends
+        each players remaining stocks will be sold automatically at their current market price and give
+        each player their final cash totals. The player with the most money at the end, wins.
+      </p>
+    
+  </div>
+
 </template>
 
 <script>
@@ -8,6 +31,10 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+div{
+  margin: 10%;
+}
 
 </style>
